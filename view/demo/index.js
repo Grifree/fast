c1fbd3d2d06b88e27ab0c929fafcd53da51340c6
nimojs <nimo.jser@gmail.com>
2016-04-17 21:59:30 +0800

    业务代码相关规范说明

--- view/demo/index.js
+++ view/demo/index.js
@@ -1,9 +1,18 @@
+// 所有模块均使用 require 引用
+// npm install 安装的模块直接引用模块名
 var $ = require('jquery')
+// 项目中的模块使用相对路径
 var loading = require('../../m/loading/')
 $(function () {
+    // 不允许直接选择 $('.main') 而是使用 .js- 前缀选择
+    // 虽然 .js- 是 class 但绝不允许在 css 中定义 .js- 前缀的样式
     var $main = $('.js-main')
+    // [name]--[status] 是 css 状态的命名方式
     $main.addClass('main--on')
 
+    // ID 一律大写，这样可以通过变量名区分选择的是 class 还是 id
+    // $loadingBtn = $('.js-loading-btn')
+    // $LoadingBtn = $('#LoadingBtn')
     var $LoadingBtn = $('#LoadingBtn')
     $LoadingBtn.on('click', function () {
         loading.show()
