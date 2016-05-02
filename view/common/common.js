d1a364de23869484438abf25a8156cf5f1bf843d
nimojs <nimo.jser@gmail.com>
2016-05-03 00:10:01 +0800

    view/common/common.js 只编写业务逻辑代码

--- view/common/common.js
+++ view/common/common.js
@@ -1,7 +1,8 @@
 var $ = require('jquery')
 $(function () {
-    // commonjs 中的选择器只允许以 #common  .common- 最为前缀
-    $('#commonDemo').on('click', function () {
-        $(this).toggleClass('m-nav--night')
-    })
+    var hour = new Date().getHours()
+    if (hour > 18 || hour < 6) {
+        // commonjs 中的选择器只允许以 #common  .common- 最为前缀
+        $('#commonDemo').addClass('m-nav--night')
+    }
 })
