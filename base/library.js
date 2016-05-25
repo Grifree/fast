21e990cfe5b0c388e9d585d0b0901a1273d93f44
nimojs <nimo.jser@gmail.com>
2016-05-25 14:24:01 +0800

    add __YOU_NEED_REQUIRE_USE_MODULE

--- base/library.js
+++ base/library.js
@@ -1,5 +1,17 @@
+window.__YOU_NEED_REQUIRE_USE_MODULE = {}
 __inline('../vendor/jquery/1.12.3/jquery.min.js')
+window.$ = null;
 __inline('../vendor/classnames/2.2.3/index.min.js')
 __inline('../vendor/react/0.14.8/react.js')
 __inline('../vendor/react/0.14.8/react-dom.min.js')
 __inline('../vendor/rem_layout/rem.js')
+
+;(function () {
+    var globalVariables = ['jQuery','React','ReactDOM','classNames']
+    var i = 0;
+    for (;i < globalVariables.length; i++) {
+        var variableName = globalVariables[i]
+        window.__YOU_NEED_REQUIRE_USE_MODULE[variableName] = window[variableName]
+        window[variableName] = null
+    }
+})();
