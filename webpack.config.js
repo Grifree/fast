775ff068c77012c6fadd56357e3ed6d01f7fb4bd
Nimo <nimo.jser@gmail.com>
2016-06-01 22:33:08 +0800

    add transform-decorators-legacy

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,8 @@ module.exports = {
                 test: /\.js$/,
                 loader: 'babel-loader',
                 query: {
-                    presets: ['es2015', 'react']
+                    presets: ['es2015', 'react'],
+                    plugins: ["transform-decorators-legacy"]
                 }
             },
             {
