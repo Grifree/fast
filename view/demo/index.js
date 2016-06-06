597c40821bfa30e153338cebe2e610bc367f9e21
Nimo <nimo.jser@gmail.com>
2016-06-06 14:49:39 +0800

    禁止使用 input[name=]

--- view/demo/index.js
+++ view/demo/index.js
@@ -1,6 +1,7 @@
 var $ = require('jquery')
 $(function () {
     // view/url/*.js 中的选择器只允许使用 "#" 和 ".js-" 作为前缀
+    // 绝对不允许使用 input[name='some'] 选择器
     $('.js-tag .tag-item-remove').on('click', function () {
         var $this = $(this)
         $this.closest('.js-tag').remove()
