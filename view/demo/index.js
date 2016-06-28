01e9c352c8116bb1f47f8b6da76a5fa87c417ae3
nimojs <nimo.jser@gmail.com>
2016-06-28 13:22:26 +0800

    inline style

--- view/demo/index.js
+++ view/demo/index.js
@@ -7,3 +7,17 @@ $(function () {
         $this.closest('.js-tag').remove()
     })
 })
+
+!(function (content) {
+    var head = document.getElementsByTagName('head')[0] ||
+    document.documentElement
+    var sty = document.createElement('style');
+    sty.type = 'text/css';
+    if (sty.styleSheet) {
+       sty.styleSheet.cssText = content;
+    }
+    else {
+       sty.innerHTML = content;
+    }
+    head.appendChild(sty);
+})(__inline("./index.scss"));
