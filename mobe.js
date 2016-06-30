ccc786a5802acd015764ccf967481e5db1f63ff1
nimojs <nimo.jser@gmail.com>
2016-06-30 11:50:03 +0800

    add php server

--- mobe.js
+++ mobe.js
@@ -1,5 +1,24 @@
+var exec = require('exec')
+exec('php -S 127.0.0.1:2009 -t view/mock/', function(err, out, code) {
+  if (err instanceof Error)
+    throw err;
+  process.stderr.write(err);
+  process.stdout.write(out);
+  process.exit(code);
+});
+
 var app = require('fms')
 app.run({
     static: './output',
-    port: '2000'
+    port: '2000',
+    view: {
+        server: 'http://127.0.0.1:2009'
+    }
+})
+
+
+app.view({
+    title: '演示',
+    url: '/demo/',
+    template: 'demo/index.html'
 })
