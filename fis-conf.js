dcb03058385978bb5adf2ac11422fff8750101d6
nimojs <nimo.jser@gmail.com>
2016-07-05 17:38:08 +0800

    delete tab

--- fis-conf.js
+++ fis-conf.js
@@ -31,23 +31,23 @@ var conf = {
             }
         },
         template: `<!DOCTYPE html>
-        <html lang="en">
-        <head>
-        <meta charset="UTF-8">
-        <meta name="renderer" content="webkit">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" ></meta>
-        <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0,user-scalable=no" />
-        <!--<script src="/vendor/rem_layout/rem.js"></script>-->
-        <link rel="stylesheet" href="/base/normalize.less">
-        <link rel="stylesheet" href="/view/common/common.less">
-        <title> <%- title %></title>
-        </head>
-        <body>
-        <script src="/base/library.js"></script>
-        <script src="/view/common/common.pack.js"></script>
-        <%- content %>
-        </body>
-        </html>`
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="renderer" content="webkit">
+<meta http-equiv="X-UA-Compatible" content="IE=edge" ></meta>
+<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0,user-scalable=no" />
+<!--<script src="/vendor/rem_layout/rem.js"></script>-->
+<link rel="stylesheet" href="/base/normalize.less">
+<link rel="stylesheet" href="/view/common/common.less">
+<title> <%- title %></title>
+</head>
+<body>
+<script src="/base/library.js"></script>
+<script src="/view/common/common.pack.js"></script>
+<%- content %>
+</body>
+</html>`
     },
     webpack: {
         devtool: 'source-map',
