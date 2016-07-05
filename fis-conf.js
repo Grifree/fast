9ae0cf0a5bfe2a888f1c8522ba284a24054187b8
nimojs <nimo.jser@gmail.com>
2016-07-05 17:36:22 +0800

    fis-conf 可以监听文件修改后自动重启

--- fis-conf.js
+++ fis-conf.js
@@ -30,7 +30,24 @@ var conf = {
                 }
             }
         },
-        template: require('fs').readFileSync(__dirname + "/m/template.ejs", 'utf-8').toString()
+        template: `<!DOCTYPE html>
+        <html lang="en">
+        <head>
+        <meta charset="UTF-8">
+        <meta name="renderer" content="webkit">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" ></meta>
+        <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0,user-scalable=no" />
+        <!--<script src="/vendor/rem_layout/rem.js"></script>-->
+        <link rel="stylesheet" href="/base/normalize.less">
+        <link rel="stylesheet" href="/view/common/common.less">
+        <title> <%- title %></title>
+        </head>
+        <body>
+        <script src="/base/library.js"></script>
+        <script src="/view/common/common.pack.js"></script>
+        <%- content %>
+        </body>
+        </html>`
     },
     webpack: {
         devtool: 'source-map',
