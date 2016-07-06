bc3f2b1ecf1cd8709f85c6fec277a9b70034e57d
nimojs <nimo.jser@gmail.com>
2016-07-06 15:24:07 +0800

    vendor/ => base/vendor/

--- base/library.js
+++ base/library.js
@@ -1,3 +1,3 @@
-__inline('../vendor/jquery/1.12.3/jquery.min.js')
-__inline('../vendor/react/0.14.8/react.js')
-__inline('../vendor/react/0.14.8/react-dom.min.js')
+__inline('./vendor/jquery/1.12.3/jquery.min.js')
+__inline('./vendor/react/0.14.8/react.js')
+__inline('./vendor/react/0.14.8/react-dom.min.js')
